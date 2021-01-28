f7ac6b9f0c638f96927db4e86f1ca6dfa0ae22bb
MarcosRoosevelt <marcosroosevelt11@gmail.com>
2021-01-28 07:42:40 -0300

    Added a new Unit

---
+++ src/UnitAgenda.pas
@@ -0,0 +1,24 @@
+unit UnitAgenda;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TFrmAgenda = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmAgenda: TFrmAgenda;
+
+implementation
+
+{$R *.fmx}
+
+end.
